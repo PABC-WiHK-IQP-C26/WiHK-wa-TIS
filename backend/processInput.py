6ae7734803717202818fd31028f1484fb12e165d
a-little-black-cat <berenis.tekin@gmail.com>
2026-01-26 17:24:30 +0800

    just some notes for later development

--- backend/processInput.py
+++ backend/processInput.py
@@ -4,6 +4,9 @@ from spacy import displacy
 
 from spacy.lang.en import English
 #also import trad chinese at some point
+import rapidfuzz # this is to do fuzzy matching if needed ,, necessarily for typos or different spelling conventions people may be used to
+import NLTK # text processing library
+import getData
 
 def process_text(input_text):
     print("Processing text with spaCy NLP model...", flush=True)
@@ -17,3 +20,18 @@ def process_text(input_text):
 
     for entity in doc_en.ents:
         print(entity.text, entity.label_) 
+
+    # rapidfuzz can be used to do a word or sentence by sentence similarity check with the overview and itinerary fields from the tours data fetched from Google Sheets.
+
+
+
+    """
+    The idea is to tokenize client input, identify key entities (e.g., locations, dates), relevant information regarding location etc.
+    Primarily use the overview and itinerary fields from the tours data fetched from Google Sheets.
+    https://spotintelligence.com/2022/12/19/text-similarity-python/ 
+    >>TF_IDF approach for text similarity
+    """
+
+
+
+
